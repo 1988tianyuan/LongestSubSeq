3f197ba1881f7703e931dbeb8bfb659a21d42516
liugeng <liu.geng@navercorp.com>
2019-04-02 11:22:33 +0800

    minPathSum

--- src/Test.java
+++ src/Test.java
@@ -10,9 +10,10 @@ public class Test {
     private static Random random = new Random();
     private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss");
     public static void main(String[] args){
-        int[] nums = new int[]{2,7,9,3,1};
-        HouseRobber robber = new HouseRobber();
-        System.out.println(robber.rob(nums));
+//        int[][] nums = new int[][]{new int[]{1,3,1}, new int[]{1,5,1},new int[]{4,2,1}};
+        int[][] nums = new int[][]{new int[]{1,3,1}, new int[]{1,5,1}};
+        MinPathSum minPathSum = new MinPathSum();
+        System.out.println(minPathSum.minPathSum(nums));
     }
 
 
---
+++ src/leetcode_China/MinPathSum.java
@@ -0,0 +1,40 @@
+package leetcode_China;
+
+import java.util.List;
+
+/**
+ * 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 说明：每次只能向下或者向右移动一步
+ * 输入:
+ * [
+ *   [1,3,1],
+ *   [1,5,1],
+ *   [4,2,1]
+ * ]
+ * 输出: 7
+ * 解释: 因为路径 1→3→1→1→1 的总和最小。
+ */
+public class MinPathSum {
+
+	public int minPathSum(int[][] grid) {
+		int length = grid.length;
+		int width = grid[0].length;
+		if (width == 0) {
+			return 0;
+		}
+		int[][] dp = new int[length][width];
+		dp[0][0] = grid[0][0];
+		for (int j = 0; j < length; j++) {
+			for (int i = 0; i < width; i++) {
+				if (j == 0 && i != 0) {
+					dp[0][i] = grid[0][i] + dp[0][i-1];
+				} else if (i == 0 && j != 0) {
+					dp[j][0] = grid[j][0] + dp[j-1][0];
+				} else if (i != 0){
+					dp[j][i] = grid[j][i] + Math.min(dp[j-1][i], dp[j][i-1]);
+				}
+			}
+		}
+		return dp[length-1][width-1];
+	}
+}
