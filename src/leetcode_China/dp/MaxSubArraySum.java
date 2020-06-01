e6a4ef0d53c26fba9403f678d8897aa9f0907a18
1988tianyuan <467651794@qq.com>
2020-06-01 11:59:35 +0800

    add MaxSubArraySum

---
+++ src/leetcode_China/dp/MaxSubArraySum.java
@@ -0,0 +1,30 @@
+package leetcode_China.dp;
+
+/**
+ * 输入一个整型数组，数组里有正数也有负数。数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ * 要求时间复杂度为O(n)。
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ */
+public class MaxSubArraySum {
+
+    public static int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = nums[0];
+        for (int current : nums) {
+            if (sum <= 0) {
+                sum = current;
+            } else {
+                sum += current;
+            }
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+}
