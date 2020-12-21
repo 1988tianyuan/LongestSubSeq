d70297f4a21e3c4a7a2929e30b6494bda64f7764
1988tianyuan <467651794@qq.com>
2020-12-22 00:44:18 +0800

    79-WordExist.go

--- src/leetcode_China/array/main/79-WordExist.go
+++ src/leetcode_China/array/main/79-WordExist.go
@@ -47,42 +47,55 @@ func exist(board [][]byte, word string) bool {
 	for i := 0; i < len(mark); i++ {
 		mark[i] = make([]int, len(board[i]))
 	}
-	return checkExists(board, word, 0, 0, 0, mark)
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if checkExists(board, word, j, i, 0, mark) {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func checkExists(board [][]byte, word string, x int, y int, position int, mark [][]int) bool {
-	//time.Sleep(time.Duration(100) * time.Millisecond)
-	if (y >= len(board) || y < 0) || (x >= len(board[0]) || x < 0) || position >= len(word){
-		// 达到边界返回false
-		return false
-	}
 	if mark[y][x] == 1 {
 		// 已经来过了
 		return false
 	}
+	mark[y][x] = 1
 	if board[y][x] == word[position] {
 		if position == len(word) - 1 {
 			mark[y][x] = 1
 			return true
 		}
 		position++
+	} else {
+		mark[y][x] = 0
+		return false
 	}
-	mark[y][x] = 1
-	d1 := checkExists(board, word, x + 1, y, position, mark)
-	if d1 == true {
-		return true
+	if x + 1 < len(board[0]) {
+		d1 := checkExists(board, word, x + 1, y, position, mark)
+		if d1 == true {
+			return true
+		}
 	}
-	d2 := checkExists(board, word, x - 1, y, position, mark)
-	if d2 == true {
-		return true
+	if x - 1 >= 0 {
+		d2 := checkExists(board, word, x - 1, y, position, mark)
+		if d2 == true {
+			return true
+		}
 	}
-	d3 := checkExists(board, word, x, y + 1, position, mark)
-	if d3 == true {
-		return true
+	if y + 1 < len(board) {
+		d3 := checkExists(board, word, x, y + 1, position, mark)
+		if d3 == true {
+			return true
+		}
 	}
-	d4 := checkExists(board, word, x, y - 1, position, mark)
-	if d4 == true {
-		return true
+	if y - 1 >= 0 {
+		d4 := checkExists(board, word, x, y - 1, position, mark)
+		if d4 == true {
+			return true
+		}
 	}
 	mark[y][x] = 0
 	return false
@@ -94,5 +107,24 @@ func main() {
 		{'S','F','C','S'},
 		{'A','D','E','E'},
 	}
-	fmt.Println(exist(board, "ASD"))
+	fmt.Println(exist(board, "BA"))
+	board2 := [][]byte{
+		{'a','b'},
+	}
+	fmt.Println(exist(board2, "ba"))
+	board3 := [][]byte{
+		{'F','Y','C','E','N','R','D'},
+		{'K','L','N','F','I','N','U'},
+		{'A','A','A','R','A','H','R'},
+		{'N','D','K','L','P','N','E'},
+		{'A','L','A','N','S','A','P'},
+		{'O','O','G','O','T','P','N'},
+		{'H','P','O','L','A','N','O'},
+	}
+	fmt.Println(exist(board3, "POLAND"))
+	board4 := [][]byte{
+		{'a','b'},
+		{'c','d'},
+	}
+	fmt.Println(exist(board4, "acdb"))
 }
