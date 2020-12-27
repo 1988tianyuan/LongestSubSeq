96e96920130694af65fa515be334fd9f8cfbfc6d
1988tianyuan <467651794@qq.com>
2020-12-28 00:37:36 +0800

    96-numTrees

--- src/leetcode_China/tree/main/96-numTrees.go
+++ src/leetcode_China/tree/main/96-numTrees.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
 //
 // 示例:
@@ -19,8 +21,58 @@ package main
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-func numTrees(n int) int {
+func numTreesDp(n int) int {
+	if n == 1 {
+		return 1
+	}
+	dp := make([]int, n)
+	dp[0] = 1
+	for i := 1; i <= n; i++ {
 
+	}
+}
+
+func numTrees(n int) int {
+	if n == 1 {
+		return 1
+	}
+	num := 0
+	dp := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		if i > n>>1+1 {
+			num+=dp[n-i+1]
+		} else {
+			curNum := findRootNumTrees(i, 1, n)
+			dp[i]=curNum
+			num+=curNum
+		}
+	}
+	return num
+}
+
+func findRootNumTrees(rootI int, start int, end int) int {
+	if start > end {
+		return 0
+	}
+	if start == end {
+		return 1
+	}
+	leftNum := 0
+	for i := start; i < rootI; i++ {
+		curNum := findRootNumTrees(i, start, rootI-1)
+		leftNum+=curNum
+	}
+	rightNum := 0
+	for j := end; j > rootI; j-- {
+		curNum := findRootNumTrees(j, rootI+1, end)
+		rightNum+=curNum
+	}
+	if leftNum != 0 && rightNum != 0 {
+		return leftNum * rightNum
+	}
+	return leftNum + rightNum
 }
 //leetcode submit region end(Prohibit modification and deletion)
-
+func main() {
+	fmt.Println(numTrees(5))
+}
