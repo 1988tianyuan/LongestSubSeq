34841dcfcccc6b5052a98a86e9fb392dc852ccd0
1988tianyuan <467651794@qq.com>
2021-01-24 19:12:19 +0800

    doing makeConnected

--- src/leetcode_China/array/main/1319-makeConnected.go
+++ src/leetcode_China/array/main/1319-makeConnected.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //用以太网线缆将 n 台计算机连接成一个网络，计算机的编号从 0 到 n-1。线缆用 connections 表示，其中 connections[i] = [
 //a, b] 连接了计算机 a 和 b。
 //
@@ -59,34 +61,83 @@ package main
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func makeConnected(n int, connections [][]int) int {
-	if extraLine(n, connections) == -1 {
+	if len(connections) - n + 1 < 0 {
 		return -1
 	}
-	connectedSets := make([]*ConnectedSet, 0)
+	connectedSets := make([]*ConnectedSet, 1)
+	connectedSets[0] = &ConnectedSet{}
 	connectMarks := make([]int, n)
+	setsNum := 0
 	for i := 0; i < len(connections); i++ {
 		first := connections[i][0]
 		second := connections[i][1]
-		if connectMarks[first] != 0 {
+		if connectMarks[first] != 0 && connectMarks[second] == 0 {
 			connectPos := connectMarks[first]
 			connectedSet := connectedSets[connectPos]
-
+			connectedSet.set[second] = 1
+			connectedSet.lineNum++
+			connectMarks[second] = connectPos
+		} else if connectMarks[first] == 0 && connectMarks[second] != 0 {
+			connectPos := connectMarks[second]
+			connectedSet := connectedSets[connectPos]
+			connectedSet.set[first] = 1
+			connectedSet.lineNum++
+			connectMarks[first] = connectPos
+		} else if connectMarks[first] == 0 && connectMarks[second] == 0 {
+			newConnectedSet := &ConnectedSet{lineNum: 1, set: map[int]int{}}
+			newConnectedSet.set[first] = 1
+			newConnectedSet.set[second] = 1
+			connectedSets = append(connectedSets, newConnectedSet)
+			connectMarks[first] = len(connectedSets) - 1
+			connectMarks[second] = len(connectedSets) - 1
+			setsNum++
+		} else {
+			userMark := connectMarks[first]
+			useSet := connectedSets[userMark]
+			if connectMarks[first] != connectMarks[second] {
+				noUseMark := connectMarks[second]
+				noUseSet := connectedSets[noUseMark]
+				for k := range noUseSet.set {
+					connectMarks[k] = connectMarks[first]
+					useSet.set[k] = 1
+				}
+				useSet.lineNum+=noUseSet.lineNum
+				setsNum--
+				connectedSets[noUseMark] = nil
+			}
+			useSet.lineNum++
 		}
 	}
-
+	for i := 0; i < n; i++ {
+		if connectMarks[i] == 0 {
+			newConnectedSet := &ConnectedSet{set: map[int]int{}}
+			newConnectedSet.set[i] = 1
+			connectedSets = append(connectedSets, newConnectedSet)
+			setsNum++
+		}
+	}
+	return setsNum - 1
 }
 
 type ConnectedSet struct {
-	num int
+	lineNum int
 	set map[int]int
 }
-
-func extraLine(n int, connections [][]int) int {
-	extra := n - 1 - len(connections)
-	if extra < 0 {
-		return -1
-	}
-	return extra
-}
 //leetcode submit region end(Prohibit modification and deletion)
+func main() {
+	connections := [][]int{{0,1},{0,2},{0,3},{1,2},{1,3}}
+	fmt.Println(makeConnected(6, connections))
+
+	connections2 := [][]int{{0,1},{0,2},{1,2}}
+	fmt.Println(makeConnected(4, connections2))
+
+	connections3 := [][]int{{0,1},{0,2},{3,4},{2,3}}
+	fmt.Println(makeConnected(5, connections3))
+
+	connections4 := [][]int{{1,5},{1,7},{1,2},{1,4},{3,7},{4,7},{3,5},{0,6},{0,1},{0,4},{2,6},{0,3},{0,2}}
+	fmt.Println(makeConnected(12, connections4))
+
+	connections5 := [][]int{{0,1},{0,2},{0,3},{1,3}}
+	fmt.Println(makeConnected(6, connections5))
+}
 
